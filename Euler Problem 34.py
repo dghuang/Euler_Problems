28d08e724967b8197a2531795dbf22db73d7ef78
dghuang <davidgao7@live.com>
2016-01-06 19:05:28 -0500

    Finished problem 34
    Finished problem 34

--- Euler Problem 34.py
+++ Euler Problem 34.py
@@ -3,8 +3,26 @@
 def factorial(n):
     if n == 1: return 1
     else: return n * factorial(n-1)
+    
+def digits(n):
+    s = list(str(n))
+    digits = map(int, s)
+    return digits
+
+def isCurious(n):
+    sum = 0
+    for digit in digits(n):
+        sum += fact[digit]
+    return n == sum
 
 #it's faster to have a quick reference guide than to call the factorial function every time it needs to be calculated
-fact = []
+fact = [1]
 for i in range(1, 10):
     fact.append(factorial(i))
+
+sum = 0
+for x in range(3, 2000000):
+    if isCurious(x):
+        sum += x
+
+print sum
