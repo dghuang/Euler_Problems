bd49077285d32cdd6a65d0063e0a128b2d0853f8
dghuang <davidgao7@live.com>
2016-01-19 00:27:33 -0500

    Finished problem 36

---
+++ Euler Problem 36.py
@@ -0,0 +1,21 @@
+#find sum of all numbers under a million that are palindromes in both base 10 and 2
+
+def isPalindrome(n):
+    li = list(str(n))
+    l = len(li)
+    if l == 1:
+        return True
+    else:
+        for x in range(0, l/2):
+            if li[x] != li[l-x-1]:
+                return False
+        else:
+            return True
+
+sum = 0
+for x in xrange(1, 1000000):
+    b = int(bin(x)[2:])
+    if isPalindrome(x) & isPalindrome(b):
+        sum += x
+
+print sum
